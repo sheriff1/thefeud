2be32f7415e98fe7ff9745d50489831363e34b38
sheriff1 <sheriff.jolaoso@gmail.com>
2025-06-07 08:58:41 -0400

    added console.log for testing

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -8,6 +8,7 @@ const auth = getAuth(app);
 
 if (import.meta.env.DEV) {
   connectAuthEmulator(auth, 'http://localhost:8089');
+  console.log('Connected to Firebase Auth Emulator');
 }
 
 export { auth, signInAnonymously, onAuthStateChanged };
