d19e735bee5f494a0ad33ad08b42c7ba5831ec0b
sheriff1 <sheriff.jolaoso@gmail.com>
2025-06-07 09:56:02 -0400

    fb test cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,30 +5,12 @@
 </template>
 
 <script setup lang="ts">
-// Example: in main.ts or App.vue setup
-import { auth, signInAnonymously, onAuthStateChanged } from './firebase';
+import { auth, signInAnonymously } from './firebase';
 
-console.log('About to call signInAnonymously');
-console.log('import.meta.env.MODE:', import.meta.env.MODE);
-signInAnonymously(auth)
-  .then(() => {
-    console.log('Signed in anonymously');
-  })
-  .catch((error) => {
-    console.error('Anonymous sign-in failed:', error);
-  });
-console.log('signInAnonymously() promise created');
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in anonymously
-    console.log('User ID:', user.uid);
-  }
-});
+signInAnonymously(auth);
 </script>
 
 <style>
-/* Optional: some base styling */
 body {
   margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
